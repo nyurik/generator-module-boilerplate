a6f81ab7c6debd94a1562a58e81d4ac74ad016a1
duivvv <geoffrey.dhuyvetters@gmail.com>
2016-11-21 15:21:44 +0100

    :wrench: changes to init script

--- generators/app/lib/init.js
+++ generators/app/lib/init.js
@@ -4,27 +4,20 @@ module.exports = ({author, github, name}) => {
 
   console.log();
 
-  console.log(`Hey`, author, `,`);
+  console.log(`Hey`, `${author},`);
+  console.log();
 
   console.log(`Created module`, chalk.yellow(`'${name}'`), `with generator-module-boilerplate`);
   console.log();
 
-  console.log(`use`, chalk.cyan(`npm run build:watch`), `/`, chalk.cyan(`npm run test:watch`));
-  console.log(`for development mode`);
+  console.log(`use`, chalk.cyan(`npm run build:watch`), `/`, chalk.cyan(`npm run test:watch`), `for development mode`);
 
+  console.log(`use`, chalk.cyan(`npm run build`), `for production builds (ES2015, CommonJS, UMD)`);
+
+  console.log(`use`, chalk.cyan(`npm run release (premajor|patch|minor|major)`, `for releasing`));
   console.log();
 
-  console.log(`use`, chalk.cyan(`npm run build`));
-  console.log(`for production builds (ES2015, CommonJS, UMD)`);
-
-  console.log();
-
-  console.log(`use`, chalk.cyan(`npm run release (premajor|patch|minor|major)`));
-
-  console.log();
-
-  console.log(`everything is set up to push to https://github.com/${github}/${name}`);
-
+  console.log(`everything is set up to push to https://github.com/${github}/${name}.git`);
   console.log();
 
 
